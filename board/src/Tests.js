7ceaeb7c41c5f7a2944ef0b1b8b4bbebc2a1e82a
Denis Gorodnichev <denis.gorodnichev@gmail.com>
2023-04-24 14:46:22 +0300

    react-board: collapse tests

--- board/src/Tests.js
+++ board/src/Tests.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
 
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,6 +10,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 import Status from "./Status";
 
@@ -28,7 +33,66 @@ function xhr(url) {
 	return fetch(url).then(response => response.json());
 }
 
-  
+// example https://codesandbox.io/s/s8y5gx?file=/demo.tsx:461-511
+function CollapsibleRow({tests, isStable, isOpen}) {
+    const [open, setOpen] = React.useState(isOpen);
+    const label = isStable? "stable": "unstable";
+
+    return (
+      <React.Fragment>
+        <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+          <TableCell>
+            <IconButton
+              aria-label="expand row"
+              size="small"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+            </IconButton>
+            </TableCell>
+            <TableCell component="th" scope="row">
+              {label}
+            </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Box sx={{ margin: 1 }}>
+                <Table size="small" aria-label="purchases">
+                  <TableHead>
+                    <TableCell></TableCell>
+                  {Object.keys(DASHBOARDS).map((key) => {
+                    return <TableCell key={key}>{key}</TableCell>
+                  })}
+                  </TableHead>
+                  <TableBody>
+                    {tests.map((test) => (
+                      <TableRow key={test.name}>
+                        <TableCell>
+                          {test.name}
+                        </TableCell>
+                        {
+                          test.boards.map((board) => {
+                            return (
+                              <TableCell key={board.buildUrl}>
+                                <Status board={board}/>
+                              </TableCell>
+                            )
+                          })
+                        }
+                    </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Box>
+            </Collapse>
+          </TableCell>
+        </TableRow>
+      </React.Fragment>
+    );
+  }
+
+
 function Tests() {
     const [tests, getTests] = useState([]);
 
@@ -36,22 +100,22 @@ function Tests() {
         const dashboardsPromises = Object.entries(DASHBOARDS).map(async (e) => {
             const [dashboardId, views] = e;
             const viewPromises = views.map(async (viewUrl) => {
-                const response = await xhr(viewUrl + "/api/json?tree=jobs[name,url]");
-                return response["jobs"].map(job => [job.name, job.url]);
+                const response = await xhr(viewUrl + "/api/json?tree=jobs[name,url,color]");
+                return response["jobs"].map(job => [job.name, job.url, job.color]);
             });
-    
+
             const viewValues = await Promise.all(viewPromises);
-            const allValues = viewValues.reduce((acc, value) => {
+            const boardTests = viewValues.reduce((acc, value) => {
                 acc = acc || {};
                 value.forEach((item, i) => {
-                    const [name, url] = item;
-                    acc[name] = url;
+                    const [name, url, color] = item;
+                    acc[name] = {"url": url, "color": color};
                 });
-    
+
                 return acc;
             }, {});
-    
-            return [dashboardId, allValues];
+
+            return [dashboardId, boardTests];
         });
 
         const dashboardsValues = await Promise.all(dashboardsPromises);
@@ -74,22 +138,43 @@ function Tests() {
             return acc;
         }, {});
 
+        const stability2tests = Object.entries(test2dashboard).reduce((acc, e) => {
+          const [testname, dashboardIds] = e;
+          const unstableTests = dashboardIds.filter((dashboardId) => {
+            const testColor = dashboards[dashboardId][testname].color;
+            return testColor !== 'blue';
+          });
+
+          const isStable = unstableTests.length === 0;
+          const tests = [testname, ...acc[isStable] || []];
+          acc[isStable] = tests;
+          return acc;
+        }, {});
+
         const dashboardIds = Object.keys(dashboards);
 
-        const data = Object.entries(test2dashboard).map((e) => {
-            const [testName, boards] = e;
+        const data = Object.entries(stability2tests).reduce((acc, e) => {
+          const [isStable, tests] = e;
+
+          const testsData = tests.map((testName) => {
+            const boards = test2dashboard[testName];
 
             const boardsData = dashboardIds.map((dashboardId) => {
                 if (boards.includes(dashboardId)) {
-                    const buildUrl = dashboards[dashboardId][testName];
+                    const buildUrl = dashboards[dashboardId][testName].url;
                     const imageUrl = buildUrl + "badge/icon";
-                    return {"buildUrl": buildUrl, "imageUrl": imageUrl};
+                    const color = dashboards[dashboardId][testName].color;
+                    return {"buildUrl": buildUrl, "imageUrl": imageUrl, "color": color};
                 }
                 return null;
             });
 
             return {"name": testName, "boards": boardsData};
-        });
+          });
+
+          acc[isStable] = testsData;
+          return acc;
+        }, {});
         return getTests(data);
     };
 
@@ -101,31 +186,16 @@ function Tests() {
     return (
         <TableContainer component={Paper}>
             <Table size={'small'}>
-                <TableHead>
-                <TableRow>
-                    <TableCell></TableCell>
-                    {Object.keys(DASHBOARDS).map((key) => {
-                        return <TableCell key={key}>{key}</TableCell>
-                    })}
-                </TableRow>
-                </TableHead>
                 <TableBody>
-                {tests.map((row) => {
-                     return <TableRow
-                     key={row.name}>
-                        <TableCell scope="row">
-                             {row.name}
-                        </TableCell>
-                        {
-                        row.boards.map((board) => {
-                             return (
-                                <TableCell key={board.buildUrl}>
-                                    <Status board={board}/>
-                                </TableCell>
-                             )
-                            })
-                        }
-                    </TableRow>
+                {Object.entries(tests).map((e) => {
+                  const [isStableValue, data] = e;
+
+                  const isStable = isStableValue !== "false";
+                  const isOpen = !isStable || tests.length === 1;
+
+                  return (
+                    <CollapsibleRow tests={data} isStable={isStable} isOpen={isOpen} />
+                  )
                 })}
                 </TableBody>
             </Table>
