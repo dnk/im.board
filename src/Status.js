5a22903c07d6c85f4564cc556fa9eb7ae002d7ee
Denis Gorodnichev <denis.gorodnichev@gmail.com>
2024-02-23 19:07:29 +0200

    use github pages

--- src/Status.js
+++ src/Status.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
+function fix_url(url) {
+    url = url.replace("https://jenkins.com.int.zone", "https://dashboard.cloud-blue.website/jenkins"); //"/jenkins");
+    return url;
+  }
+
 function Status({board}) {
     if (!board) {
         return "";
     }
 
     return <a href={board.buildUrl} target='_blank' rel='nofollow noopener noreferrer'>
-            <img src={board.imageUrl} />
+            <img src={fix_url(board.imageUrl)} />
         </a>
 }
 
