245f69f6cded123258f738cf7a30531ce9880378
Denis Gorodnichev <denis.gorodnichev@gmail.com>
2025-01-22 18:34:13 +0200

    IDP/release-5.0

--- src/Releases.js
+++ src/Releases.js
@@ -1,102 +1,101 @@
-import Table from '@mui/material/Table';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import Table from "@mui/material/Table";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 import Status from "./Status";
-import { TableBody, TableHead } from '@mui/material';
+import { TableBody, TableHead } from "@mui/material";
 
 const CB_VERSIONS = ["unstable", "21.19", "21.18"];
-const IDP_VERSIONS = ["master", "release-4.2", "release-4.1"]
-const UAM_VERSIONS = ["master", "release-3.0", "release-2.1"]
-
+const IDP_VERSIONS = ["master", "release-5.0", "release-4.2"];
+const UAM_VERSIONS = ["master", "release-3.0", "release-2.1"];
 
 const COMPONENTS = {
-    "OSS": CB_VERSIONS.map(version => {
-        return {
-            "buildUrl": `https://jenkins.com.int.zone/job/oss/job/${version}/job/validate-and-promote/`
-        }
-    }),
-    "BSS": CB_VERSIONS.map(version => {
-        return {
-            "buildUrl": `https://jenkins.com.int.zone/job/bss/job/${version}/job/validate-and-promote/`
-        }
-    }),
-    "Branding UI Cluster": CB_VERSIONS.map(version => {
-        return {
-            "buildUrl": `https://jenkins.com.int.zone/job/branding-ui-cluster/job/${version}/job/validate-and-promote/`
-        }
-    }),
-    "IDP": IDP_VERSIONS.map(version => {
-        return {
-            "buildUrl": `https://jenkins.com.int.zone/job/idp-backend/job/${version}/job/validate-and-promote/`
-        }
-    }),
-    "UAM": UAM_VERSIONS.map(version => {
-        return {
-            "buildUrl": `https://jenkins.com.int.zone/job/uam/job/${version}/job/validate-and-promote/`
-        }
-    }),
-    "GDPR": {
-        "buildUrl": "https://jenkins.com.int.zone/job/gdpr-backend/job/master/job/validate-and-promote/",
-    },
-    "E2E SDK": CB_VERSIONS.map(version => {
-        return {
-            "buildUrl": `https://jenkins.com.int.zone/job/e2e-tests-v2/job/${version}/job/validate-and-promote/`
-        }
-    })
+  OSS: CB_VERSIONS.map((version) => {
+    return {
+      buildUrl: `https://jenkins.com.int.zone/job/oss/job/${version}/job/validate-and-promote/`,
+    };
+  }),
+  BSS: CB_VERSIONS.map((version) => {
+    return {
+      buildUrl: `https://jenkins.com.int.zone/job/bss/job/${version}/job/validate-and-promote/`,
+    };
+  }),
+  "Branding UI Cluster": CB_VERSIONS.map((version) => {
+    return {
+      buildUrl: `https://jenkins.com.int.zone/job/branding-ui-cluster/job/${version}/job/validate-and-promote/`,
+    };
+  }),
+  IDP: IDP_VERSIONS.map((version) => {
+    return {
+      buildUrl: `https://jenkins.com.int.zone/job/idp-backend/job/${version}/job/validate-and-promote/`,
+    };
+  }),
+  UAM: UAM_VERSIONS.map((version) => {
+    return {
+      buildUrl: `https://jenkins.com.int.zone/job/uam/job/${version}/job/validate-and-promote/`,
+    };
+  }),
+  GDPR: {
+    buildUrl:
+      "https://jenkins.com.int.zone/job/gdpr-backend/job/master/job/validate-and-promote/",
+  },
+  "E2E SDK": CB_VERSIONS.map((version) => {
+    return {
+      buildUrl: `https://jenkins.com.int.zone/job/e2e-tests-v2/job/${version}/job/validate-and-promote/`,
+    };
+  }),
 };
 
-
 function Releases() {
+  return (
+    <TableContainer component={Paper} key="releases-table-container">
+      <Table
+        sx={{
+          [`& .${tableCellClasses.root}`]: {
+            border: "none",
+          },
+        }}
+        size="small"
+        key="releases-table"
+      >
+        <TableHead key="releases-head">
+          <TableRow>
+            <TableCell key="releases-head-name"></TableCell>
+            <TableCell key="releases-head-unstable">Unstable</TableCell>
+            <TableCell key="releases-head-realeases">Releases</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody key="releases-body">
+          {Object.entries(COMPONENTS).map(([name, components]) => {
+            return (
+              <TableRow key={name}>
+                <TableCell>{name}</TableCell>
+                {(Array.isArray(components) ? components : [components]).map(
+                  (component, index) => {
+                    const buildStatus = {
+                      buildUrl: component.buildUrl,
+                      imageUrl:
+                        component.buildUrl +
+                        `badge/icon?&subject=\${params.BUILD_NAME}`,
+                    };
 
-    return (
-        <TableContainer component={Paper} key="releases-table-container">
-            <Table sx={{
-                [`& .${tableCellClasses.root}`]: {
-                    border: "none"
-                }
-            }} size="small" key='releases-table'>
-                <TableHead key='releases-head'>
-                    <TableRow>
-                        <TableCell key='releases-head-name'></TableCell>
-                        <TableCell key='releases-head-unstable'>Unstable</TableCell>
-                        <TableCell key='releases-head-realeases'>Releases</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody key='releases-body'>
-                    {
-                        Object.entries(COMPONENTS).map(([name, components]) => {
-                            return (
-                                <TableRow key={name}>
-                                    <TableCell>
-                                        {name}
-                                    </TableCell>
-                                    {
-                                        (Array.isArray(components) ? components : [components])
-                                            .map((component, index) => {
-                                                const buildStatus = {
-                                                    "buildUrl": component.buildUrl,
-                                                    "imageUrl": component.buildUrl + `badge/icon?&subject=\${params.BUILD_NAME}`
-                                                };
+                    const key = `${name}-${index}`;
 
-                                                const key = `${name}-${index}`;
-
-                                                return (
-                                                    <TableCell key={key} >
-                                                        <Status board={buildStatus} key={key + "-status"} />
-                                                    </TableCell>
-                                                );
-                                            })
-                                    }
-                                </TableRow>
-                            )
-                        })
-                    }
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+                    return (
+                      <TableCell key={key}>
+                        <Status board={buildStatus} key={key + "-status"} />
+                      </TableCell>
+                    );
+                  },
+                )}
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 export default Releases;
--- src/Tests.js
+++ src/Tests.js
@@ -1,16 +1,23 @@
-import React, { useState, useEffect, useMemo } from 'react'
+import React, { useState, useEffect, useMemo } from "react";
 
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableContainer from '@mui/material/TableContainer';
-import Paper from '@mui/material/Paper';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableContainer from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
 
-import { Stack, TableCell, TableHead, TableRow, TableSortLabel, tableCellClasses } from '@mui/material';
-import Status from './Status';
-import MaterialUISwitch from './MaterialUISwitch';
+import {
+  Stack,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+  tableCellClasses,
+} from "@mui/material";
+import Status from "./Status";
+import MaterialUISwitch from "./MaterialUISwitch";
 
 const DASHBOARDS = {
-  "unstable": [
+  unstable: [
     "https://jenkins.com.int.zone/view/Tests/view/master/view/abondarenko/",
     "https://jenkins.com.int.zone/view/Tests/view/master/view/eoktyabrskiy/",
     "https://jenkins.com.int.zone/view/Tests/view/master/view/igarro/",
@@ -20,8 +27,8 @@ const DASHBOARDS = {
     "https://jenkins.com.int.zone/view/Tests/view/master/view/vkopchenin/",
 
     "https://jenkins.com.int.zone/job/idp-backend/job/master/job/tests/job/master/view/%20 All-launches",
+    "https://jenkins.com.int.zone/job/idp-backend/job/release-5.0/job/tests/job/master/view/%20 All-launches",
     "https://jenkins.com.int.zone/job/idp-backend/job/release-4.2/job/tests/job/master/view/%20 All-launches",
-    "https://jenkins.com.int.zone/job/idp-backend/job/release-4.1/job/tests/job/master/view/%20 All-launches",
 
     "https://jenkins.com.int.zone/job/uam/job/master/job/tests/job/master/view/%20 All-launches",
     "https://jenkins.com.int.zone/job/uam/job/release-3.0/job/tests/job/master/view/%20 All-launches",
@@ -43,8 +50,8 @@ const DASHBOARDS = {
     "https://jenkins.com.int.zone/view/Tests/view/21/view/vkopchenin/",
 
     "https://jenkins.com.int.zone/job/idp-backend/job/master/job/tests/job/21/view/%20 All-launches",
+    "https://jenkins.com.int.zone/job/idp-backend/job/release-5.0/job/tests/job/21/view/%20 All-launches",
     "https://jenkins.com.int.zone/job/idp-backend/job/release-4.2/job/tests/job/21/view/%20 All-launches",
-    "https://jenkins.com.int.zone/job/idp-backend/job/release-4.1/job/tests/job/21/view/%20 All-launches",
 
     "https://jenkins.com.int.zone/job/uam/job/master/job/tests/job/21/view/%20 All-launches",
     "https://jenkins.com.int.zone/job/uam/job/release-3.0/job/tests/job/21/view/%20 All-launches",
@@ -60,15 +67,17 @@ const DASHBOARDS = {
 
 const TEST_NAME_CORRECTIONS = {
   //'IDP : upgrade-idp-backend': 'IDP : idp-upgrade'
-}
+};
 
 function fix_url(url) {
-  url = url.replace("https://jenkins.com.int.zone", "https://dashboard.cloud-blue.online/jenkins");
+  url = url.replace(
+    "https://jenkins.com.int.zone",
+    "https://dashboard.cloud-blue.online/jenkins",
+  );
   return url;
 }
 
 function comporator(field, order) {
-
   function nameComporator(a, b) {
     return a.testName.localeCompare(b.testName);
   }
@@ -77,17 +86,17 @@ function comporator(field, order) {
     const aWeight = a.weightHolder.weight;
     const bWeight = b.weightHolder.weight;
     if (aWeight === bWeight) {
-      return 0
+      return 0;
     } else {
       return aWeight > bWeight ? -1 : 1;
     }
   }
 
-  const sign = order === 'asc' ? 1 : -1;
+  const sign = order === "asc" ? 1 : -1;
 
   return (a, b) => {
     switch (field) {
-      case 'name': {
+      case "name": {
         const result = nameComporator(a, b);
         if (result === 0) {
           return weightComporator(a, b) * sign;
@@ -95,7 +104,7 @@ function comporator(field, order) {
         return result * sign;
       }
 
-      case 'weight':
+      case "weight":
       default: {
         const result = weightComporator(a, b);
         if (result === 0) {
@@ -107,8 +116,6 @@ function comporator(field, order) {
     }
   };
 }
-
-
 
 async function xhr(url) {
   url = fix_url(url);
@@ -122,23 +129,32 @@ async function xhr(url) {
 }
 
 async function fetchSvgText(buildUrl, preferStableBuild) {
-  const urlStable = new URL(fix_url(buildUrl) + 'badge/icon');
+  const urlStable = new URL(fix_url(buildUrl) + "badge/icon");
   urlStable.searchParams.append("link", `${buildUrl}/\${buildId}`);
-  urlStable.searchParams.append("build", 'last:${params.BUILD_NAME=}');
+  urlStable.searchParams.append("build", "last:${params.BUILD_NAME=}");
 
-  const urlComponent = new URL(fix_url(buildUrl) + 'badge/icon');
+  const urlComponent = new URL(fix_url(buildUrl) + "badge/icon");
   urlComponent.searchParams.append("link", `${buildUrl}/\${buildId}`);
-  urlComponent.searchParams.append("subject", '${params.COMPONENT_NAME}-${params.BUILD_NAME}');
+  urlComponent.searchParams.append(
+    "subject",
+    "${params.COMPONENT_NAME}-${params.BUILD_NAME}",
+  );
 
   const prferableUrl = preferStableBuild ? urlStable : urlComponent;
 
-  const preferableResponsePromise = fetch(prferableUrl).then(response => response.text());
+  const preferableResponsePromise = fetch(prferableUrl).then((response) =>
+    response.text(),
+  );
   const preferableResponseText = await preferableResponsePromise;
 
-  const isPreferebleNOK = preferableResponseText.includes("not run</text>") || preferableResponseText.includes("-</text>")
+  const isPreferebleNOK =
+    preferableResponseText.includes("not run</text>") ||
+    preferableResponseText.includes("-</text>");
   if (isPreferebleNOK) {
     const alternativeUrl = !preferStableBuild ? urlStable : urlComponent;
-    const alternativeResponsePromise = fetch(alternativeUrl).then(response => response.text());
+    const alternativeResponsePromise = fetch(alternativeUrl).then((response) =>
+      response.text(),
+    );
     const alternativeResponseText = await alternativeResponsePromise;
 
     // const isAlternativeNOK = alternativeResponseText.includes("not run</text>") || alternativeResponseText.includes('>params.'); // there is unresolved param in alternative response. not applicable for when stable is alternative
@@ -146,7 +162,9 @@ async function fetchSvgText(buildUrl, preferStableBuild) {
     //   return [prferableUrl, preferableResponseText];
     // } else {
     if (alternativeResponseText.includes("not run</text>")) {
-      return preferStableBuild ? [prferableUrl, preferableResponseText.replace('/buildId', '')] : [alternativeUrl, alternativeResponseText.replace('/buildId', '')];
+      return preferStableBuild
+        ? [prferableUrl, preferableResponseText.replace("/buildId", "")]
+        : [alternativeUrl, alternativeResponseText.replace("/buildId", "")];
     } else {
       return [alternativeUrl, alternativeResponseText];
     }
@@ -158,18 +176,19 @@ async function fetchSvgText(buildUrl, preferStableBuild) {
 
 function getStatus(svgText) {
   const running = svgText.includes(">running</text>");
-  const stable = svgText.includes('fill="#44cc11"/>') || svgText.includes(">not run</text>");
-  return { "running": running, "stable": stable };
+  const stable =
+    svgText.includes('fill="#44cc11"/>') || svgText.includes(">not run</text>");
+  return { running: running, stable: stable };
 }
 
 const CORE_COMPONENTS = ["OSS", "BSS", "BRANDING"];
 function jobName(name, url) {
-  const parts = url.split('/job/');
+  const parts = url.split("/job/");
   const branch = parts[2];
   const branch_parts = branch.split("-");
   const version = branch_parts[branch_parts.length - 1];
 
-  const group = parts[1].split('-')[0].toUpperCase();
+  const group = parts[1].split("-")[0].toUpperCase();
   if (CORE_COMPONENTS.includes(group)) {
     return `${group} : ${name}`;
   } else {
@@ -180,21 +199,22 @@ function jobName(name, url) {
 function toRows(tests) {
   const rows = Object.entries(tests).map(([testName, boards]) => {
     const weightHolder = {
-      weight: -1
+      weight: -1,
     };
 
     const statuses = Object.entries(boards).map(([dashboardId, data]) => {
-      const rowKeyTemplate = testName + '-' + dashboardId;
+      const rowKeyTemplate = testName + "-" + dashboardId;
 
       if (data) {
-        const weight = (data.status.stable ? 0 : 10) + (data.status.running ? 5 : 0);
+        const weight =
+          (data.status.stable ? 0 : 10) + (data.status.running ? 5 : 0);
         weightHolder.weight = Math.max(weightHolder.weight, weight);
         const board = {
-          "svgText": data.svgText,
-          "imageUrl": data.url,
-          "buildUrl": data.buildUrl,
-        }
-        return <Status key={rowKeyTemplate + '-status'} board={board} />;
+          svgText: data.svgText,
+          imageUrl: data.url,
+          buildUrl: data.buildUrl,
+        };
+        return <Status key={rowKeyTemplate + "-status"} board={board} />;
       } else {
         return "";
       }
@@ -203,7 +223,7 @@ function toRows(tests) {
     return {
       testName: testName,
       status: statuses,
-      weightHolder: weightHolder
+      weightHolder: weightHolder,
     };
   });
 
@@ -213,11 +233,11 @@ function toRows(tests) {
 function Tests() {
   const [loaded, setLoaded] = useState(false);
   const [rows, setRows] = useState([]);
-  const defaultSort = { "field": "weight", "order": "asc" };
+  const defaultSort = { field: "weight", order: "asc" };
   const [sort, setSort] = useState(defaultSort);
   const sortComporator = useMemo(
     () => comporator(sort.field, sort.order),
-    [sort]
+    [sort],
   );
   const [amountOfFailedTests, setAmountOfFailedTests] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(0);
@@ -226,34 +246,37 @@ function Tests() {
     const dashboardsPromises = Object.entries(DASHBOARDS).map(async (e) => {
       const [dashboardId, views] = e;
       const viewPromises = views.map((viewUrl) => {
-        let url = (viewUrl + "/api/json?tree=jobs[name,url,color]");
+        let url = viewUrl + "/api/json?tree=jobs[name,url,color]";
         return xhr(url).then((response) => {
           const jobs = response["jobs"] || [];
           const promises = jobs.map(async (job) => {
             let name = jobName(job.name, job.url);
             name = TEST_NAME_CORRECTIONS[name] || name;
 
-            const preferStableBuild = dashboardId === 'unstable';
-            const [url, svgText] = await fetchSvgText(job.url, preferStableBuild);
+            const preferStableBuild = dashboardId === "unstable";
+            const [url, svgText] = await fetchSvgText(
+              job.url,
+              preferStableBuild,
+            );
             const status = getStatus(svgText);
 
             return [name, url, job.url, svgText, status];
           });
           return Promise.all(promises);
-        })
+        });
       });
 
-      const viewTests = (await Promise.all(viewPromises));
+      const viewTests = await Promise.all(viewPromises);
 
       const boardTests = viewTests.reduce((acc, value) => {
         acc = acc || {};
         value.forEach((item) => {
           const [name, url, buildUrl, svgText, status] = item;
           acc[name] = {
-            "url": url,
-            "buildUrl": buildUrl,
-            "svgText": svgText,
-            "status": status
+            url: url,
+            buildUrl: buildUrl,
+            svgText: svgText,
+            status: status,
           };
         });
 
@@ -273,29 +296,34 @@ function Tests() {
         boards[dashboardId] = data;
         acc[testName] = boards;
         return acc;
-      }, acc)
+      }, acc);
 
       return acc;
     }, {});
 
-    const amountOfFailedTests = Object.entries(dashboardTests).reduce((acc, [name, tests]) => {
-      const amount = Object.entries(tests).reduce((acc, [_, data]) => {
-        if (acc !== 0) {
-          return acc;
-        } else {
-          const amount = data.status.stable ? 0 : 1
-          return acc + amount;
-        }
-      }, 0);
+    const amountOfFailedTests = Object.entries(dashboardTests).reduce(
+      (acc, [name, tests]) => {
+        const amount = Object.entries(tests).reduce((acc, [_, data]) => {
+          if (acc !== 0) {
+            return acc;
+          } else {
+            const amount = data.status.stable ? 0 : 1;
+            return acc + amount;
+          }
+        }, 0);
 
-      return acc + amount;
-    }, 0);
+        return acc + amount;
+      },
+      0,
+    );
 
     setAmountOfFailedTests(amountOfFailedTests);
     const rows = toRows(dashboardTests);
     setRows(rows);
-    setRowsPerPage(amountOfFailedTests !== 0 ? amountOfFailedTests : rows.length);
-  };
+    setRowsPerPage(
+      amountOfFailedTests !== 0 ? amountOfFailedTests : rows.length,
+    );
+  }
 
   useEffect(() => {
     if (!loaded) {
@@ -306,14 +334,14 @@ function Tests() {
 
   const visibleRows = useMemo(
     () =>
-      rows.toSorted(sortComporator)
+      rows
+        .toSorted(sortComporator)
         .map((row) => {
           return [row.testName, ...row.status];
         })
         .slice(0, rowsPerPage),
-    [rows, rowsPerPage, sortComporator]
+    [rows, rowsPerPage, sortComporator],
   );
-
 
   if (!loaded || rows.length === 0) {
     return false;
@@ -323,67 +351,78 @@ function Tests() {
 
   return (
     <TableContainer component={Paper} key="tests-TableContainer">
-      <Table size="small" sx={{
-        [`& .${tableCellClasses.root}`]: {
-          border: "none"
-        }
-      }} key="tests-Table">
+      <Table
+        size="small"
+        sx={{
+          [`& .${tableCellClasses.root}`]: {
+            border: "none",
+          },
+        }}
+        key="tests-Table"
+      >
         <TableHead key="tests-head">
           <TableRow>
             <TableCell key="tests-name">
-              <TableSortLabel active={sort.field === "name"} direction={sort.field === "name" ? sort.order : 'asc'} onClick={
-                () => {
-                  const currentDirection = sort.field === "name" ? sort.order : 'desc';
-                  const order = currentDirection === 'desc' ? 'asc' : 'desc';
-                  setRowsPerPage(rows.length)
-                  setSort({ "field": "name", "order": order })
-                }
-              }>
+              <TableSortLabel
+                active={sort.field === "name"}
+                direction={sort.field === "name" ? sort.order : "asc"}
+                onClick={() => {
+                  const currentDirection =
+                    sort.field === "name" ? sort.order : "desc";
+                  const order = currentDirection === "desc" ? "asc" : "desc";
+                  setRowsPerPage(rows.length);
+                  setSort({ field: "name", order: order });
+                }}
+              >
                 <Stack direction="row" spacing={1} alignItems="center">
-                  <MaterialUISwitch size='small' onChange={() => {
-                    if (rowsPerPage === amountOfFailedTests) {
-                      setRowsPerPage(rows.length)
-                    } else {
-                      setSort(defaultSort);
-                      setRowsPerPage(amountOfFailedTests);
-                    }
-                  }}
+                  <MaterialUISwitch
+                    size="small"
+                    onChange={() => {
+                      if (rowsPerPage === amountOfFailedTests) {
+                        setRowsPerPage(rows.length);
+                      } else {
+                        setSort(defaultSort);
+                        setRowsPerPage(amountOfFailedTests);
+                      }
+                    }}
                     disabled={amountOfFailedTests === 0}
                     checked={rowsPerPage !== amountOfFailedTests}
                   />
                 </Stack>
               </TableSortLabel>
             </TableCell>
-            {
-              dashboards.map((key) => {
-                return <TableCell key={key}>
-                  <TableSortLabel active={sort.field === key} direction={sort.field === key ? sort.order : 'asc'} onClick={
-                    () => {
-                      const currentDirection = sort.field === key ? sort.order : 'desc';
-                      const order = currentDirection === 'desc' ? 'asc' : 'desc';
-                      setRowsPerPage(rows.length)
-                      setSort({ "field": key, "order": order })
-                    }
-                  }>{key}</TableSortLabel>
+            {dashboards.map((key) => {
+              return (
+                <TableCell key={key}>
+                  <TableSortLabel
+                    active={sort.field === key}
+                    direction={sort.field === key ? sort.order : "asc"}
+                    onClick={() => {
+                      const currentDirection =
+                        sort.field === key ? sort.order : "desc";
+                      const order =
+                        currentDirection === "desc" ? "asc" : "desc";
+                      setRowsPerPage(rows.length);
+                      setSort({ field: key, order: order });
+                    }}
+                  >
+                    {key}
+                  </TableSortLabel>
                 </TableCell>
-              })
-            }
+              );
+            })}
           </TableRow>
         </TableHead>
         <TableBody key="tests-body">
-          {
-            visibleRows.map((row) => {
-              return (
-                <TableRow>
-                  {
-                    row.map((column) => {
-                      return <TableCell>{column}</TableCell>;
-                    })
-                  }
-                </TableRow>
-              )
-            })
-          }
+          {visibleRows.map((row) => {
+            return (
+              <TableRow>
+                {row.map((column) => {
+                  return <TableCell>{column}</TableCell>;
+                })}
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
     </TableContainer>
